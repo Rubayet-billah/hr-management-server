43434781344f56316a314075c3836e563c2d8abe
Rubayet-billah <rubayetbillah909@gmail.com>
2022-12-19 21:03:58 +0600

    test

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const cors = require('cors');
+require('dotenv').config()
 
 const app = express();
 const port = process.env.PORT || 5000;
@@ -7,6 +8,8 @@ const port = process.env.PORT || 5000;
 // middle wares
 app.use(cors())
 app.use(express.json())
+
+
 
 app.get('/', async (req, res) => {
     res.send('HR Management Server is running fine')
