8c5272c8817c0c916f0005baf6b0526a58afebe8
Md Maruf Hossain <mickeymaruf@gmail.com>
2023-01-03 22:51:29 +0600

    verify admin middleware added

--- routes/Admins.js
+++ routes/Admins.js
@@ -6,19 +6,29 @@ const router = express.Router();
 const client = require('../db/mongodb');
 const adminsCollection = client.db('HrManager').collection('admins');
 
+// middlewares
+const verifyIsSuperAdmin = async (req, res, next) => {
+    const email = req.query.email;
+    const isSuperAdmin = await adminsCollection.findOne({ email, isSuperAdmin: true });
+    if (!isSuperAdmin) {
+        return res.send({ status: 404, message: 'unauthorized access' });
+    }
+    next();
+}
+
 router.get('/', async (req, res) => {
     const query = {};
     const admins = await adminsCollection.find(query).toArray();
     res.send(admins);
 });
 
-router.post('/', async (req, res) => {
+router.post('/', verifyIsSuperAdmin, async (req, res) => {
     const admin = req.body;
     const insertAdminResult = await adminsCollection.insertOne(admin);
     res.send(insertAdminResult);
 });
 
-router.delete('/:id', async (req, res) => {
+router.delete('/:id', verifyIsSuperAdmin, async (req, res) => {
     const id = req.params.id;
     const query = { _id: ObjectId(id) };
     const result = await adminsCollection.deleteOne(query);
